a32cab5364ce5561bd2b287bf732ffe4b7c18139
ChihoLays <89733221+ChihoLays@users.noreply.github.com>
2024-03-15 18:23:17 +0700

    Update mdp.py

--- mdp.py
+++ mdp.py
@@ -39,38 +39,3 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
     cv2.destroyAllWindows()
 
 mp_drawing.DrawingSpec
-
-def calculate_angle(a, b, c):
-        a = np.array(a)  # First
-        b = np.array(b)  # Mid
-        c = np.array(c)  # End
-
-        radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-        angle = np.abs(radians * 180.0 / np.pi)
-
-        if angle > 180.0:
-            angle = 360 - angle
-        return int(angle)
-
-        elbow_l = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-        shoulder_l = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-        hip_l = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-#
-        image_height, image_width, _ = image.shape
-
-        #coordinate_NOSE
-        x_coodinate_NOSE = results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width
-        y_coodinate_NOSE = results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height
-
-    #coordinate_LEFT_SHOULDER
-        x_coodinate_LEFT_SHOULDER = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x * image_width
-        y_coodinate_LEFT_SHOULDER = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y * image_height
-
-        coodinate_LEFT_SHOULDER = [x_coodinate_LEFT_SHOULDER,y_coodinate_LEFT_SHOULDER]
-
-        print(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].x)
-        print(coodinate_LEFT_SHOULDER)
-
-
-        print(image.shape)
-        print([image_height,image_width])
